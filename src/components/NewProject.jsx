d74cad2b49a0450e18b2d98b3bbf6e1ea03e2e8c
Nate Ensign <dixienate@gmail.com>
2023-11-08 17:30:36 -0700

    complete up to being able to add a new project and see it in the console

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import Input from './Input'
+import React, { useRef } from "react";
+import Input from "./Input";
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave () {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const eneteredDueDate = description.current.value;
+
+    onAdd({
+        title: enteredTitle,
+        description: enteredDescription,
+        dueDate: eneteredDueDate
+    })
+  }
+
   return (
-    <div className='w-[35rem] mt-16 '>
-        <menu className='flex items-center justify-end gap-4 my-4'>
-            <li><button className='text-stone-800 hover: text-stone-950'>Cancel</button></li>
-            <li><button className='bg-stone-800 text-stone-50 hover:bg-stone-600 px-6 py-2 rounded-md'>Save</button></li>
-        </menu>
-        <div>
-            <Input label='Title' />
-            <Input label="Description" textarea />
-            <Input label="Due Date" />
-        </div>
+    <div className="w-[35rem] mt-16 ">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover: text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button onClick={handleSave} className="bg-stone-800 text-stone-50 hover:bg-stone-600 px-6 py-2 rounded-md">
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} label="Due Date" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewProject
+export default NewProject;
